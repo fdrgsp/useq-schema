8cd87c4ec99b861c7172b6cfeab501508f223591
fdrgsp <federico.gasparoli@gmail.com>
2023-01-16 18:06:44 -0500

    fix: tile_name in tiles

--- src/useq/_tile.py
+++ src/useq/_tile.py
@@ -64,7 +64,9 @@ class TileRelative(TilePlan):
     Attributes
     ----------
     tile_name : str
-        Name of the tile.
+        Name of the tile. It will be added as a prefix name for each
+        tile positions.
+        (e.g. tile_name = 'Grid' -> `Grid_Pos000`, `Grid_Pos001`)
     rows: int
         Number of rows.
     cols: int
@@ -98,6 +100,8 @@ class TileRelative(TilePlan):
     # def tiles(self, name_prefix: str = "") -> Sequence[Position]:
     def tiles(self) -> Sequence[Position]:
         """Generate the position list."""
+        prefix = f"{self.tile_name}_" if self.tile_name else ""
+    
         x_pos, y_pos, z_pos = self.start_coords
         cam_width, cam_height = self.camera_roi
         overlap_x = cam_width - (cam_width * self.overlap_x) / 100
@@ -131,7 +135,7 @@ class TileRelative(TilePlan):
                     if c == 0:
                         y_pos -= increment_y
                     tile_pos_list.append(Position(
-                        name=f"{self.tile_name}Pos{pos_count:03d}", x=x_pos, y=y_pos, z=z_pos
+                        name=f"{prefix}Pos{pos_count:03d}", x=x_pos, y=y_pos, z=z_pos
                     ))
                     pos_count += 1
                     if col > 0:
@@ -142,7 +146,7 @@ class TileRelative(TilePlan):
                     if r > 0 and c == 0:
                         y_pos -= increment_y
                     tile_pos_list.append(Position(
-                        name=f"{self.tile_name}Pos{pos_count:03d}", x=x_pos, y=y_pos, z=z_pos
+                        name=f"{prefix}Pos{pos_count:03d}", x=x_pos, y=y_pos, z=z_pos
                     ))
                     pos_count += 1
                     if c < self.cols - 1:
